b60e5912ab44cc7c7e96412fb5fa86a18689ad30
Dearie <eburujacintha@gmail.com>
2025-05-01 03:54:13 +0400

    added needed components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import "tailwindcss";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './global.css';
-// import Navbar from './components/Navbar';
 import PropertyNavbar from './components/PropertyNavbar';
 import PropertyHome from './pages/PropertyHome';
 import PropertyDetail from './pages/PropertyDetail';
 
+
+import RegistrationChoice from './components/RegistrationChoice';
+import LandlordRegistration from './pages/LandlordRegistration';
+import PropertySeekerRegistration from './pages/PropertySeekerRegistration';
+import Login from './pages/Login';
+
+
+
+
+
 const App = () => {
   return (
-          <Router>
-            {/* <Navbar /> */}
-            <Routes>
-              <Route path="/" element={<PropertyHome />} />
-              <Route path="/properties" element={<PropertyHome />} />
-              <Route path="/properties-detail" element={<PropertyDetail />} />
-            </Routes>
-          </Router>
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<PropertyHome />} />
+        <Route path="/properties" element={<PropertyHome />} />
+        <Route path="/properties-detail" element={<PropertyDetail />} />
+        
+        {/* Auth routes */}
+        <Route path="/register" element={<RegistrationChoice />} />
+        <Route path="/register/landlord" element={<LandlordRegistration />} />
+        <Route path="/register/seeker" element={<PropertySeekerRegistration />} />
+        <Route path="/login" element={<Login />} />
+        
+        
+        {/* Fallback route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   )
 }
 
